64febd863c1ed0062e062e90d04971a5215a849f
Zakai Hamilton <zakaihamilton@gmail.com>
2017-07-08 21:21:48 +0300

    bug fix

--- packages/ui/ui_scroll.js
+++ packages/ui/ui_scroll.js
@@ -156,7 +156,7 @@ package.ui.scroll = function UIScroll(me) {
                     width: object.offsetWidth,
                     height: object.offsetHeight
                 };
-                me.ui.property.broadcast(value, "ui.theme.add", "transition");
+                me.ui.property.broadcast(object, "ui.theme.add", "transition");
                 var scroll_method = function (e) {
                     var track_region = me.ui.rect.absolute_region(object.parentNode);
                     var thumb_region = me.ui.rect.absolute_region(object);
@@ -175,7 +175,7 @@ package.ui.scroll = function UIScroll(me) {
                 var release_method = function (e) {
                     removeEventListener('mousemove', scroll_method);
                     removeEventListener('mouseup', release_method);
-                    me.ui.property.broadcast(value, "ui.theme.remove", "transition");
+                    me.ui.property.broadcast(object, "ui.theme.remove", "transition");
                 };
                 addEventListener('mousemove', scroll_method);
                 addEventListener('mouseup', release_method);
