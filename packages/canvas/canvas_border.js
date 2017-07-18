84e747697c104c34aa1946b79996877e3e19f0c4
Zakai Hamilton <zakaihamilton@gmail.com>
2017-07-18 20:08:42 +0300

    fix for canvas border

--- packages/canvas/canvas_border.js
+++ packages/canvas/canvas_border.js
@@ -1,21 +1,49 @@
 /*
-    @author Zakai Hamilton
-    @component CanvasBorder
-*/
+ @author Zakai Hamilton
+ @component CanvasBorder
+ */
 
 package.canvas.border = function CanvasBorder(me) {
-    me.attach = function(object) {
+    me.attach = function (object) {
 
     };
-    me.draw = function(object, canvas) {
+    me.draw = function (object, canvas) {
         var context = me.get(canvas, "context");
-        if(object.style.borderColor) {
+        if (object.style.borderColor) {
             var region = me.ui.rect.relative_region(object, canvas);
+            var colors = me.ui.style.values(object.style.borderColor);
+            var widths = me.ui.style.values(object.style.borderWidth);
+            console.log("colors: " + JSON.stringify(colors));
+            console.log("widths: " + JSON.stringify(widths));
+            context.save();
+            context.lineJoin = "miter";
+            var lineWidth = parseInt(widths.left);
+            console.log("lineWidth:" + lineWidth);
+            var left = region.left - (lineWidth/2);
+            var right = region.left + (lineWidth/2);
+            var top = region.top - (lineWidth/2);
+            var bottom = region.top + (lineWidth/2);
+            var width = region.width;
+            var height = region.height;
+            
+            console.log("left: " + left + " top: " + top + " right: " + right + " bottom: " + bottom);
+            
+            me.trapezoid(context, colors.top, left, top, right + width, top, left + width, bottom, right, bottom);
+            me.trapezoid(context, colors.right, right + width, top, right + width, bottom + height, left + width, top + height, left + width, bottom);
+            me.trapezoid(context, colors.bottom, right + width, bottom + height, left, bottom + height, right, top + height, left + width, top + height);
+            me.trapezoid(context, colors.left, left, bottom + height, left, top, right, bottom, right, top + height);
+            context.restore();
             context.beginPath();
-            context.strokeStyle=object.style.borderColor;
-            context.lineWidth=parseInt(object.style.borderWidth);
-            context.rect(region.left,region.top,region.width,region.height);
-            context.stroke();
         }
+    };
+    me.trapezoid = function (context, color, x1, y1, x2, y2, x3, y3, x4, y4) {
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.lineTo(x3, y3);
+        context.lineTo(x4, y4);
+        context.closePath();
+        context.fillStyle = color;
+        context.fill();
     };
 };
--- packages/ui/ui_style.js
+++ packages/ui/ui_style.js
@@ -25,7 +25,37 @@ package.ui.style = function UIStyle(me) {
                         object.style[property] = value;
                     }
                 }
-            }
+            };
         }
+    };
+    me.values = function(values) {
+        var result = {left:0,top:0,right:0,bottom:0};
+        if(!values || !(typeof values === 'string' || values instanceof String)) {
+            return result;
+        }
+        var array = values.split(" ");
+        switch(array.length) {
+            case 1:
+                result.left = result.top = result.right = result.bottom = array[0];
+                break;
+            case 2:
+                result.left = result.right = array[0];
+                result.top = result.bottom = array[1];
+                break;
+            case 3:
+                result.top = array[0];
+                result.left = result.right = array[1];
+                result.bottom = array[2];
+                break;
+            case 4:
+                result.top = array[0];
+                result.right = array[1];
+                result.bottom = array[2];
+                result.left = array[3];
+                break;
+            default:
+                break;
+        }
+        return result;
     };
 };
