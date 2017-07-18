afdeaab6b15447b86f24fcc45d8164f5e7dfb4e9
Zakai Hamilton <zakaihamilton@gmail.com>
2017-07-18 20:46:29 +0300

    background canvas component

---
+++ packages/canvas/canvas_background.js
@@ -0,0 +1,18 @@
+/*
+    @author Zakai Hamilton
+    @component CanvasBackground
+*/
+
+package.canvas.background = function CanvasBackground(me) {
+    me.attach = function(object) {
+
+    };
+    me.draw = function(object, canvas) {
+        var context = me.get(canvas, "context");
+        if(object.style.background) {
+            var region = me.ui.rect.relative_region(object, canvas);
+            context.fillStyle=object.style.background;
+            context.fillRect(region.left,region.top,region.width,region.height);
+        }
+    };
+};
