6b21b3dfc1bff6c3482de7b552c7ae4e24841c07
Zakai Hamilton <zakaihamilton@gmail.com>
2017-08-03 00:34:37 +0300

    bug fix

--- packages/ui/ui_layout.js
+++ packages/ui/ui_layout.js
@@ -4,128 +4,148 @@
  */
 
 package.ui.layout = function UILayout(me) {
-    me.content = function(target) {
-        if(target && target.component === me.widget.container.id) {
+    me.content = function (target) {
+        if (target && target.component === me.widget.container.id) {
             target = me.widget.container.content(target);
         }
         return target;
     };
-    me.move = function(source, target) {
+    me.move = function (source, target) {
         target = me.content(target);
         do {
             var widget = source.firstChild;
-            if(widget) {
+            if (widget) {
                 target.appendChild(widget);
             }
-        } while(widget);
+        } while (widget);
     };
-    me.clear = function(target) {
+    me.clear = function (target) {
         target = me.content(target);
         do {
             var widget = target.firstChild;
-            if(widget) {
+            if (widget) {
                 target.removeChild(widget);
             }
-        } while(widget);
+        } while (widget);
     };
-    me.prepare = function(source, target) {
+    me.prepare = function (source, target) {
         target = me.content(target);
         do {
             var widget = target.firstChild;
-            if(widget) {
+            if (widget) {
                 var tagName = widget.tagName.toLowerCase();
-                if(tagName === "div") {
+                if (tagName === "div") {
                     do {
                         var childWidget = widget.firstChild;
-                        if(childWidget) {
+                        if (childWidget) {
                             source.appendChild(childWidget);
                         }
-                    } while(childWidget);
+                    } while (childWidget);
                     target.removeChild(widget);
-                }
-                else {
+                } else {
                     source.appendChild(widget);
                 }
             }
-        } while(widget);
+        } while (widget);
     };
-    me.pageSize = function(target) {
+    me.pageSize = function (target) {
         var container = me.ui.node.container(target, me.widget.container.id).parentNode;
         var region = me.ui.rect.relative_region(container);
         var pageHeight = container.offsetHeight - region.top;
         var pageWidth = container.offsetWidth;
-        return {width:pageWidth,height:pageHeight};
+        return {width: pageWidth, height: pageHeight};
     };
-    me.reflow = function(source, target, usePages, pageClass, columnCount=1) {
+    me.reflow = function (source, target, usePages, pageClass, columnCount = 1) {
         target = me.content(target);
         me.prepare(source, target);
         var pageSize = me.pageSize(target);
-        if(!source.firstChild) {
+        if (!source.firstChild) {
             return;
         }
         var pageIndex = 1;
         var page = null;
-        if(usePages) {
+        if (usePages) {
             page = me.createPage(target, pageClass, pageSize.width, pageSize.height, pageIndex, columnCount);
         }
         var previousWidget = null;
-        for(;;) {
+        for (; ; ) {
             var widget = source.firstChild;
-            if(!widget) {
+            if (!widget) {
                 break;
             }
-            if(page) {
-                page.appendChild(widget);
+            var location = page;
+            if(!location) {
+                location = target;
             }
-            else {
-                target.appendChild(widget);
+            if (widget.style.order) {
+                location.insertBefore(widget, me.widgetByOrder(location, widget.style.order));
+            } else {
+                location.appendChild(widget);
+            }
+            if(!page) {
                 continue;
             }
             var newPage = false;
-            if(page.scrollHeight > page.clientHeight || page.scrollWidth > page.clientWidth) {
+            if (page.scrollHeight > page.clientHeight || page.scrollWidth > page.clientWidth) {
                 newPage = true;
             }
-            if(!(widget.innerHTML || widget.firstChild)) {
+            if (!(widget.innerHTML || widget.firstChild)) {
                 page.removeChild(widget);
                 widget = null;
             }
-            if(newPage) {
-                if(widget) {
+            if (newPage) {
+                if (widget) {
                     page.removeChild(widget);
                 }
                 pageIndex++;
                 page = me.createPage(target, pageClass, pageSize.width, pageSize.height, pageIndex, columnCount);
-                if(previousWidget && previousWidget.tagName.toLowerCase().match(/h\d/)) {
+                if (previousWidget && previousWidget.tagName.toLowerCase().match(/h\d/)) {
                     page.appendChild(previousWidget);
                 }
-                if(widget) {
+                if (widget) {
                     page.appendChild(widget);
                 }
                 previousWidget = null;
-            }
-            else if(widget) {
+            } else if (widget) {
                 previousWidget = widget;
             }
         }
         me.createBreak(target);
         me.createBreak(target);
     };
-    me.createPage = function(target, pageClass, pageWidth, pageHeight, pageIndex, columnCount) {
+    me.widgetByOrder = function (page, order) {
+        var widget = page.firstChild;
+        var match = null;
+        for (; ; ) {
+            if (!widget) {
+                break;
+            }
+            if (widget.style.order) {
+                if (parseInt(widget.style.order) > parseInt(order)) {
+                    match = widget;
+                    break;
+                }
+            }
+            widget = widget.nextSibling;
+        }
+        return match;
+    };
+    me.createPage = function (target, pageClass, pageWidth, pageHeight, pageIndex, columnCount) {
         target = me.content(target);
         var page = me.ui.element.create({
-            "ui.basic.tag":"div",
-            "ui.theme.class":pageClass,
-            "ui.style.width":pageWidth + "px",
-            "ui.style.height":pageHeight + "px",
-            "ui.style.display":"block",
-            "ui.style.columnCount":columnCount,
-            "ui.style.columnGap":"100px"
+            "ui.basic.tag": "div",
+            "ui.theme.class": pageClass,
+            "ui.style.width": pageWidth + "px",
+            "ui.style.height": pageHeight + "px",
+            "ui.style.display": "block",
+            "ui.style.columnCount": columnCount,
+            "ui.style.columnGap": "100px"
         }, target);
         return page;
     };
-    me.createBreak = function(target) {
+    me.createBreak = function (target) {
         var page = me.ui.element.create({
-            "ui.basic.tag":"br"
+            "ui.basic.tag": "br"
         }, target);
         return page;
     };
