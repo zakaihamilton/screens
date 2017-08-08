df5e8a3e2086b93d1bda8aff8195b62aa248aefa
Zakai Hamilton <zakaihamilton@gmail.com>
2017-08-08 21:02:03 +0300

    added footer status bar for windows

--- packages/app/app_transform.js
+++ packages/app/app_transform.js
@@ -12,7 +12,7 @@ package.app.transform = function AppTransform(me) {
         me.singleton = me.ui.element.create(__json__, "desktop", "self");
         me.link("update", "app.transform.update", true, me.singleton);
         me.link("scrolled", "app.transform.scrolled", true, me.singleton.var.layout);
-        me.set(me.singleton, "app.transform.convert");
+        me.set(me.singleton, "app.transform.transform");
     };
     me.init = function () {
         me.prevLanguage = null;
@@ -32,10 +32,10 @@ package.app.transform = function AppTransform(me) {
             scrollPos:0
         });
         me.options.autoScroll = false;
-        me.translation = me.ui.property.toggleOptionSet(me, "doTranslation", me.convert.set);
-        me.addStyles = me.ui.property.toggleOptionSet(me, "addStyles", me.convert.set);
-        me.keepSource = me.ui.property.toggleOptionSet(me, "keepSource", me.convert.set);
-        me.showHtml = me.ui.property.toggleOptionSet(me, "showHtml", me.convert.set);
+        me.translation = me.ui.property.toggleOptionSet(me, "doTranslation", me.transform.set);
+        me.addStyles = me.ui.property.toggleOptionSet(me, "addStyles", me.transform.set);
+        me.keepSource = me.ui.property.toggleOptionSet(me, "keepSource", me.transform.set);
+        me.showHtml = me.ui.property.toggleOptionSet(me, "showHtml", me.transform.set);
         me.showInput = me.ui.property.toggleOptionSet(me, "showInput", function (options, key, value) {
             if (!me.get(me.singleton.var.layout, "ui.basic.text")) {
                 value = true;
@@ -44,20 +44,20 @@ package.app.transform = function AppTransform(me) {
         });
         me.autoScroll = me.ui.property.toggleOptionSet(me, "autoScroll", me.updateScrolling);
         me.snapToPage = me.ui.property.toggleOptionSet(me, "snapToPage", me.updateScrolling);
-        me.language = me.ui.property.choiceOptionSet(me, "language", me.convert.set);
+        me.language = me.ui.property.choiceOptionSet(me, "language", me.transform.set);
         me.fontSize = me.ui.property.choiceOptionSet(me, "fontSize", function (options, key, value) {
             me.set(me.singleton.var.layout, "ui.style.fontSize", value);
             me.set(me.singleton, "update");
         });
         me.pages = me.ui.property.toggleOptionSet(me, "pages", me.reflow.set);
         me.columns = me.ui.property.toggleOptionSet(me, "columns", me.reflow.set);
-        me.headings = me.ui.property.toggleOptionSet(me, "headings", me.convert.set);
+        me.headings = me.ui.property.toggleOptionSet(me, "headings", me.transform.set);
         me.scrollPos = me.ui.property.choiceOptionSet(me, "scrollPos");
         me.ui.theme.useStylesheet("kab.terms");
     };
     me.updateWidgets = function (showInput) {
         me.set(me.singleton.var.input, "ui.style.display", showInput ? "inline-block" : "none");
-        me.set(me.singleton.var.convert, "ui.style.display", showInput ? "inline-block" : "none");
+        me.set(me.singleton.var.transform, "ui.style.display", showInput ? "inline-block" : "none");
         me.set(me.singleton.var.layout, "ui.style.top", showInput ? "250px" : "0px");
         me.set(me.singleton.var.layout, "ui.style.borderTop", showInput ? "1px solid black" : "none");
         me.set(me.singleton.var.layout, "ui.style.fontSize", me.options.fontSize);
@@ -85,7 +85,7 @@ package.app.transform = function AppTransform(me) {
             me.updateWidgets(true);
         }
     };
-    me.convert = {
+    me.transform = {
         set: function (object) {
             var scrollPos = me.options.scrollPos;
             me.ui.layout.clear(me.singleton.var.layout);
@@ -97,10 +97,12 @@ package.app.transform = function AppTransform(me) {
                     language = me.core.string.language(text);
                     console.log("detected language: " + language);
                 }
-                me.set(me.singleton.var.spinner, "ui.style.display", "block");
+                me.set(me.singleton.var.spinner, "ui.style.visibility", "visible");
+                var beforeConversion = performance.now();
+                me.set(me.singleton.var.footer, "ui.style.display", "block");
+                me.set(me.singleton.var.footer, "ui.basic.text", "Transforming...");
                 me.kab.terms.setLanguage(function () {
                     me.kab.terms.parse(function (text) {
-                        me.set(me.singleton.var.spinner, "ui.style.display", "none");
                         if (me.prevLanguage) {
                             me.set(me.singleton.var.output, "ui.theme.remove", me.prevLanguage);
                         }
@@ -114,6 +116,12 @@ package.app.transform = function AppTransform(me) {
                         me.ui.layout.move(me.singleton.var.output, me.singleton.var.layout);
                         me.options.scrollPos = scrollPos;
                         me.set(me.singleton, "update");
+                        me.set(me.singleton.var.spinner, "ui.style.visibility", "hidden");
+                        var afterConversion = performance.now();
+                        me.set(me.singleton.var.footer, "ui.basic.text", "Transformation took " + (afterConversion - beforeConversion).toFixed() + " milliseconds");
+                        setTimeout(function() {
+                            me.set(me.singleton.var.footer, "ui.basic.text", "");
+                        }, 5000);
                     }, text, me.options);
                 }, language);
             }
