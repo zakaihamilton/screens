b935953193a1d607e2a5eb903cfbdddffca5e37c
Zakai Hamilton <zakaihamilton@gmail.com>
2017-08-23 15:12:13 +0300

    added phase numbers

--- packages/kab/kab_terms.js
+++ packages/kab/kab_terms.js
@@ -464,6 +464,9 @@ package.kab.terms = function KabTerms(me) {
         if(heading) {
             html += "<span class=\"kab-term-heading\">" + heading + "</span>";
         }
+        if(phase && phase !== "none") {
+            html += "<span class=\"kab-term-phase-number kab-term-phase-number-" + phase + "\"></span>";
+        }
         html += text;
         if(phase || tooltip || heading) {
             html += "</span>";
