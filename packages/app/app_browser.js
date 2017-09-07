ebd4812bd36146e3af2657aafe6e0fb559381228
Zakai Hamilton <zakaihamilton@gmail.com>
2017-09-07 19:55:44 +0300

    bug fix

--- packages/app/app_browser.js
+++ packages/app/app_browser.js
@@ -11,6 +11,9 @@ package.app.browser = function AppBrowser(me) {
         set: function(object) {
             var window = me.widget.window.window(object);
             var url = me.get(window.var.url, "text");
+            if(!url.startsWith("http://")) {
+                url = "http://" + url;
+            }
             me.set(window.var.embed, "ui.basic.src", url);
         }
     };
