d0319400a0058b1bd49ad5611255daa1af1479e2
Zakai Hamilton <zakaihamilton@gmail.com>
2017-11-26 18:13:25 +0200

    can convert files using ffmpeg

--- packages/media/media_ffmpeg.js
+++ packages/media/media_ffmpeg.js
@@ -3,10 +3,29 @@
  @component MediaFFMpeg
  */
 
+package.require("media.ffmpeg", "server");
+
 package.media.ffmpeg = function MediaFFMpeg(me) {
     me.init = function() {
         const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
         me.ffmpeg = require('fluent-ffmpeg');
         me.ffmpeg.setFfmpegPath(ffmpegPath);
+    };
+    me.convert = function(callback, source, format, target) {
+        me.ffmpeg(source)
+        .toFormat(format)
+        .on('error', function (err) {
+            me.package.core.console.log('An error occurred: ' + err.message);
+            callback(err);
+        })
+        .on('progress', function (progress) {
+            me.package.core.console.log('Processing: ' + source + ' to ' +  target + ' ' + progress.targetSize + ' KB converted = ' + JSON.stringify(progress));
+            callback(progress);
+        })
+        .on('end', function () {
+            me.package.core.console.log('Processing finished for ' + source + ' to ' + target);
+            callback(null);
+        })
+        .save(target);
     };
 };
--- packages/platform/browser.js
+++ packages/platform/browser.js
@@ -106,6 +106,9 @@ function startBrowser(appName, appArgs) {
         ],
         "manager":[
             "download"
+        ],
+        "media":[
+            "ffmpeg"
         ]
     },
             function (info) {
