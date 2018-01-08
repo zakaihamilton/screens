2ece9df85c7aad6bee1e7076fc2e43bbbacee8db
Zakai Hamilton <zakaihamilton@gmail.com>
2018-01-08 18:38:44 +0200

    added datastore verification support

--- packages/code/storage/storage_data.js
+++ packages/code/storage/storage_data.js
@@ -12,14 +12,24 @@ package.storage.data = function StorageData(me) {
                 me.datastore = require('@google-cloud/datastore');
                 me.core.console.log("retrieving version information");
                 me.core.util.version(version => {
+                    var date = new Date();
+                    var id = date.getTime();
                     var data = {
                         date: Date(),
                         version: version,
                         port: me.core.http.port
                     };
-                    me.core.console.log("startup: " + JSON.stringify(data));
-                    me.save(me.core.console.error, data, "startup");
-                    me.unlock(task);
+                    me.core.console.log("startup: " + JSON.stringify(data) + " id: " + id);
+                    me.saveAndVerify(err => {
+                        if (err) {
+                            me.core.console.log("failed to save startup to cloud: " + err.message);
+                            me.unlock(task);
+                        }
+                        else {
+                            me.core.console.log("startup verification complete");
+                        }
+                        me.unlock(task);
+                    }, data, "startup", id);
                 });
             }, "npm rebuild");
         });
@@ -34,7 +44,7 @@ package.storage.data = function StorageData(me) {
         });
         callback(me.service);
     };
-    me.save = function (callback, value, type = null, id = null) {
+    me.save = function (callback, value, type, id) {
         me.getService((service) => {
             const key = service.key([type, id]);
             service.save({
@@ -45,12 +55,35 @@ package.storage.data = function StorageData(me) {
             });
         });
     };
-    me.load = function (callback, type = null, id = null) {
+    me.load = function (callback, type, id) {
         me.getService((service) => {
             const key = service.key([type, id]);
             service.get(key, function (err, value) {
                 callback(err, value);
             });
         });
+    };
+    me.verify = function (callback, value, type, id) {
+        me.load((err, compare) => {
+            callback(err, JSON.stringify(compare) === JSON.stringify(value), value);
+        }, type, id);
+    };
+    me.saveAndVerify = function (callback, value, type, id) {
+        me.save(err => {
+            if (err) {
+                callback(err);
+            }
+            me.verify((err, result, compare) => {
+                if (err) {
+                    callback(err);
+                } else if (!result) {
+                    var message = "verification mismatch between: " + JSON.stringify(data) + " and " + JSON.stringify(compare);
+                    var error = new Error(message);
+                    callback(error);
+                } else {
+                    callback(null);
+                }
+            }, value,type, id);
+        }, value, type, id);
     };
 };
