e4cdc6ea9ee7fab195620dba7f031057437ba955
Zakai Hamilton <zakaihamilton@gmail.com>
2018-01-17 08:53:56 +0200

    update profile function

--- packages/code/user/user_profile.js
+++ packages/code/user/user_profile.js
@@ -5,6 +5,7 @@
 
 package.require("user.profile", "server");
 package.user.profile = function UserProfile(me) {
+    me.nonIndexed = [];
     me.add = function(callback, name, password) {
         if(!name) {
             callback(new Error("profile name not provided"), null);
@@ -71,6 +72,41 @@ package.user.profile = function UserProfile(me) {
                 callback(null, profile);
             }
         }, "user.profile.data", name);
+    };
+    me.profile = function(callback, name) {
+        me.storage.data.load((err, profile) => {
+            if(err) {
+                err = new Error("cannot retrieve " + name + " profile: " + err.message);
+            }
+            else if(!profile) {
+                err = new Error(name + " is an empty profile");
+            }
+            else if(profile.name !== name) {
+                err = new Error(name + " does not match name in profile: " + profile.name);
+            }
+            if(err) {
+                callback(err, null);
+            }
+            else {
+                callback(null, profile);
+            }
+        }, "user.profile.data", name);
+    };
+    me.update = function(callback, profile) {
+        var err = null;
+        if(!profile) {
+            var err = new Error(name + " is an empty profile");
+            callback(err);
+            return;
+        }
+        me.storage.data.save((err) => {
+            if(err) {
+                err = new Error("cannot save " + profile.name + " profile: " + err.message);
+                callback(err);
+                return;
+            }
+            callback(null);
+        }, "user.profile.data", profile.name, profile, me.nonIndexed);
     };
     me.exists = function(callback, name) {
         me.profile((err, profile) => {
