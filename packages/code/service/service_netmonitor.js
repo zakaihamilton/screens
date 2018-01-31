d47e4d33446e64a1b87fb25e3800455760408bb4
Zakai Hamilton <zakaihamilton@gmail.com>
2018-01-31 19:58:54 +0200

    added tcp tracking

--- packages/code/service/service_netmonitor.js
+++ packages/code/service/service_netmonitor.js
@@ -9,30 +9,38 @@ package.service.netmonitor = function ServiceNetMonitor(me) {
         me.core.service.config(config => {
             if(config) {
                 me.pcap = require('pcap');
+                me.tracker = new me.pcap.TCPTracker();
                 me.util = require('util');
                 var devices = config.device;
                 if(devices && !Array.isArray(devices)) {
                     devices = [devices];
                 }
                 for(var device of devices) {
-                    me.pcap_session = null;
+                    me.session = null;
                     try {
-                        me.pcap_session = me.pcap.createSession(device, config.filter);
+                        me.session = me.pcap.createSession(device, config.filter);
                     }
                     catch(e) {
                         
                     }
-                    if(me.pcap_session) {
+                    if(me.session) {
                         me.core.console.log("connected through device: " + device +  " filter: " + config.filter);
                         break;
                     }
                 }
-                if(me.pcap_session) {
-                    me.pcap_session.on('packet', function (raw_packet) {
+                if(me.session) {
+                    me.session.on('packet', function (raw_packet) {
                         var packet = me.pcap.decode.packet(raw_packet);
+                        me.tracker.track_packet(packet);
                         me.manager.packet.push(() => {
 
                         }, packet, ref);
+                    });
+                    me.tracker.on('start', function (session) {
+                        me.core.console.log("Start of TCP session between " + session.src_name + " and " + session.dst_name);
+                    });
+                    me.tracker.on('end', function (session) {
+                        me.core.console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
                     });
                 }
                 else {
