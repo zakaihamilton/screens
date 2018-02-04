65b9855d434fdcaa8df724673a03e560f1514d11
Zakai Hamilton <zakaihamilton@gmail.com>
2018-02-04 18:02:20 +0200

    Using line chart

--- packages/code/app/app_packets.js
+++ packages/code/app/app_packets.js
@@ -7,7 +7,7 @@ package.app.packets = function AppPackets(me) {
     me.launch = function (args) {
         return me.ui.element.create(__json__, "workspace", "self");
     };
-    me.init = function() {
+    me.init = function () {
         me.ui.options.load(me, null, {
             "autoRefresh": true
         });
@@ -16,7 +16,7 @@ package.app.packets = function AppPackets(me) {
     me.refresh = {
         set: function (object) {
             var window = me.widget.window.window(object);
-            if(me.options.autoRefresh) {
+            if (me.options.autoRefresh) {
                 setTimeout(() => {
                     me.core.property.set(window, "app.packets.refresh");
                 }, 1000);
@@ -33,6 +33,42 @@ package.app.packets = function AppPackets(me) {
             me.manager.packet.reset(() => {
                 me.core.property.notify(window, "app.packets.refresh");
             });
+        }
+    };
+    me.data = {
+        get: function (object) {
+            function dateNow(sec) {
+                return me.widget.chart.dateNow(sec, "d");
+            }
+            function dateNowString(sec) {
+                return me.widget.chart.dateNowString(sec, "d");
+            }
+            return [
+                [
+                    {x: dateNow(1), y: 1},
+                    {x: dateNow(2), y: 20},
+                    {x: dateNow(3), y: 50},
+                    {x: dateNow(4), y: -30},
+                    {x: dateNow(5), y: 60},
+                    {x: dateNow(6), y: 76},
+                    {x: dateNow(7), y: 32},
+                    {x: dateNow(8), y: 24},
+                    {x: dateNow(9), y: 72},
+                    {x: dateNow(10), y: 34}
+                ],
+                [
+                    {x: dateNowString(1), y: 10},
+                    {x: dateNowString(2), y: 5},
+                    {x: dateNowString(3), y: 20},
+                    {x: dateNowString(4), y: 30},
+                    {x: dateNowString(5), y: 40},
+                    {x: dateNowString(6), y: 86},
+                    {x: dateNowString(7), y: 22},
+                    {x: dateNowString(8), y: 14},
+                    {x: dateNowString(9), y: 42},
+                    {x: dateNowString(10), y: 34}
+                ]                
+            ];
         }
     };
 };
--- packages/code/widget/widget_chart.js
+++ packages/code/widget/widget_chart.js
@@ -17,7 +17,7 @@ package.widget.chart = function WidgetChart(me) {
                 me.moment = moment;
                 me.chart = chart;
                 me.unlock(task);
-            }, ['/node_modules/moment/moment.js','/node_modules/chart.js/dist/chart.js']);
+            }, ['/node_modules/moment/moment.js','/node_modules/chart.js/dist/chart.bundle.js']);
         });
     };
     me.context = {
@@ -26,20 +26,47 @@ package.widget.chart = function WidgetChart(me) {
             return context;
         }
     };
+    me.data = {
+        get: function(object) {
+            return object.data;
+        },
+        set: function(object, value) {
+            object.data = value;
+        }
+    };
     me.info = {
         get: function(object) {
             return object.chartInfo;
         },
         set: function(object, value) {
             object.chartInfo = value;
+            if(object.chartInfo.data.datasets) {
+                var datasets = object.chartInfo.data.datasets;
+                if(datasets) {
+                    for(var datasetIndex = 0; datasetIndex < datasets.length; datasetIndex++) {
+                        var dataset = datasets[datasetIndex];
+                        var match = object.data[datasetIndex];
+                        if(match) {
+                            dataset.data = match;
+                        }
+                    }
+                }
+            }
             if(object.chart) {
                 object.chart.data = object.chartInfo.data;
                 object.chart.update();
             }
             else {
                 var context = me.core.property.get(object, "context");
+                console.log("object.chartInfo:" + JSON.stringify(object.chartInfo));
                 object.chart = new me.chart.Chart(context, object.chartInfo);
             }
         }
+    };
+    me.dateNowString = function(amount, type) {
+        return me.moment().add(amount, type).format();
+    };
+    me.dateNow = function(amount, type) {
+        return me.moment().add(amount, type).toDate();
     };
 };
