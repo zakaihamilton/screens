44284934c7c783b75850863eb8763e9ee636aa91
Zakai Hamilton <zakaihamilton@gmail.com>
2018-02-14 13:32:47 +0200

    bug fix

--- packages/code/service/service_httpserver.js
+++ packages/code/service/service_httpserver.js
@@ -30,8 +30,8 @@ package.service.httpserver = function HttpServer(me) {
                 if (extension === "mp4") {
                     var mimeType = "video/mp4";
                     info.custom = true;
-                    me.manager.packet.signal(null, filePath);
                     me.core.stream.serve(info.headers, info.response, filePath, mimeType);
+                    me.manager.packet.signal(null, filePath);
                 }
                 else {
                     me.lock(info.task, task => {
