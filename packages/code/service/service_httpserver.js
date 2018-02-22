42674536d7db92faf363cd379b6d28ceccd8ea25
Zakai Hamilton <zakaihamilton@gmail.com>
2018-02-22 15:40:49 +0200

    bug fixes

--- packages/code/service/service_httpserver.js
+++ packages/code/service/service_httpserver.js
@@ -17,10 +17,10 @@ package.service.httpserver = function HttpServer(me) {
                     me.core.property.link("core.http.receive", "service.httpserver.receive", true);
                     me.filePrefix = path;
                 }
-                var signalSuffix = config.signalSuffix;
-                if(signalSuffix) {
-                    me.core.console.log("suffix for signal: " + signalSuffix);
-                    me.signalSuffix = signalSuffix;
+                var signalMatches = config.signalMatches;
+                if(signalMatches) {
+                    me.core.console.log("signal matches: " + signalMatches);
+                    me.signalMatches = signalMatches;
                 }
                 callback();
             }
@@ -31,9 +31,17 @@ package.service.httpserver = function HttpServer(me) {
             if (info.method === "GET") {
                 var filePath = me.filePrefix + info.url.substring(1);
                 var extension = me.core.path.extension(filePath);
-                if(me.signalSuffix && filePath.endsWith(me.signalSuffix)) {
-                    me.core.console.log("signal");
-                    me.core.service.sendAll("signal");
+                if(me.signalMatches && me.signalMatches.length) {
+                    var match = true;
+                    me.signalMatches.map((signalMatch) => {
+                        if(!filePath.includes(signalMatch)) {
+                            match = false;
+                        }
+                    });
+                    if(match) {
+                        me.core.console.log("signal");
+                        me.core.service.sendAll("signal");
+                    }
                 }
                 if (extension === "mp4") {
                     var mimeType = "video/mp4";
