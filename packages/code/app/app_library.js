c02c68ee583d1d7a018e4449821561fbf2bcb21e
Zakai Hamilton <zakaihamilton@gmail.com>
2018-04-15 17:56:55 +0300

    bug fixes and initial support for edit mode

--- packages/code/app/app_library.js
+++ packages/code/app/app_library.js
@@ -10,8 +10,28 @@ screens.app.library = function AppLibrary(me) {
     me.init = async function () {
         me.tagList = me.db.library.tag.list();
     };
-    me.refresh = async function() {
+    me.refresh = async function () {
         me.tagList = me.db.library.tag.list();
+    };
+    me.initOptions = async function (object) {
+        var window = me.widget.window(object);
+        me.ui.options.load(me, window, {
+            editMode: false
+        });
+        me.ui.options.toggleSet(me, "editMode", me.updateEditMode.set);
+        me.updateEditMode.set(window);
+    };
+    me.updateEditMode = {
+        get: function (object) {
+            var window = me.widget.window(object);
+            return window.options.editMode;
+        },
+        set: function (object) {
+            var window = me.widget.window(object);
+            var editMode = window.options.editMode;
+            me.core.property.set(window.var.transform, "ui.style.opacity", editMode ? "0" : "");
+            me.core.property.set([window.var.editor,window.var.delete,window.var.update], "ui.basic.show", editMode);
+        }
     };
     me.menuList = function (object, list, group) {
         var window = me.widget.window.window(object);
@@ -22,7 +42,7 @@ screens.app.library = function AppLibrary(me) {
             }
             for (var item of items) {
                 for (var key in item) {
-                    if(key === "_id" || key === "user") {
+                    if (key === "_id" || key === "user") {
                         continue;
                     }
                     names.add(key + ":" + item[key]);
@@ -33,8 +53,8 @@ screens.app.library = function AppLibrary(me) {
                     name,
                     async function () {
                         var search = me.core.property.get(window.var.search, "ui.basic.text");
-                        if(search) {
-                            if(search.includes(name)) {
+                        if (search) {
+                            if (search.includes(name)) {
                                 return;
                             }
                             search += " ";
@@ -75,8 +95,8 @@ screens.app.library = function AppLibrary(me) {
             return me.menuList(object, me.tagList, "public");
         }
     };
-    me.changedSearch = function(object) {
-        if(me.searchTimer) {
+    me.changedSearch = function (object) {
+        if (me.searchTimer) {
             clearTimeout(me.searchTimer);
         }
         me.searchTimer = setTimeout(() => {
@@ -84,18 +104,25 @@ screens.app.library = function AppLibrary(me) {
             me.search(object);
         }, 2000);
     };
-    me.search = async function(object) {
+    me.search = async function (object) {
         clearTimeout(me.searchTimer);
         var window = me.widget.window.window(object);
         var search = me.core.property.get(window.var.search, "ui.basic.text");
         var text = "";
-        if(search) {
+        if (search) {
             var list = await me.db.library.find(0, search);
-            if(list) {
+            if (list) {
                 text = list.map(item => item.text).join("<br>");
             }
         }
+        me.core.property.set(window.var.editor, "text", text);
         me.core.property.set(window.var.transform, "text", text);
         me.core.property.set(window.var.transform, "transform");
-    }
+    };
+    me.updateRecord = async function(object) {
+        
+    };
+    me.deleteRecord = async function(object) {
+
+    };
 };
