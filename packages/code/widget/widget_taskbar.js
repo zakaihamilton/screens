63267ceb049d21a26083cece29880f4e341fffd2
Zakai Hamilton <zakaihamilton@gmail.com>
2018-07-25 14:23:57 +0300

    bug fix

--- packages/code/widget/widget_taskbar.js
+++ packages/code/widget/widget_taskbar.js
@@ -13,14 +13,16 @@ screens.widget.taskbar = function WidgetTaskBar(me) {
                     "ui.basic.src": "/packages/res/icons/chat.png",
                     "ui.touch.click": "core.app.chat",
                     "ui.class.class": "chat",
-                    "ui.basic.var": "chat"
+                    "ui.basic.var": "chat",
+                    "ui.attribute.title":"Chat"
                 },
                 {
                     "ui.basic.tag": "img",
                     "ui.basic.src": "/packages/res/icons/launcher.png",
                     "ui.touch.click": "core.app.launcher",
                     "ui.class.class": "launcher",
-                    "ui.basic.var": "launcher"
+                    "ui.basic.var": "launcher",
+                    "ui.attribute.title":"Launcher"
                 }
             ]
         }
