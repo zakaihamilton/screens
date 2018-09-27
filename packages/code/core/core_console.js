bcd0684c4275637284fba2e9e2f37568bf365685
Zakai Hamilton <zakaihamilton@gmail.com>
2018-09-27 14:47:18 +0300

    forwarding errors to server

--- packages/code/core/core_console.js
+++ packages/code/core/core_console.js
@@ -8,72 +8,94 @@ screens.core.console = function CoreConsole(me) {
     me.errors = [];
     me.enabled = true;
     me.fixedSize = 5000;
-    me.clear = function() {
+    me.clear = function () {
         me.messages = [];
         me.errors = [];
     };
-    me.init = function() {
+    me.init = function () {
         screens.log = me.log;
         screens.log_error = me.log_error;
         screens.warn = me.log_warn;
-        if(me.platform === "browser") {
+        if (me.platform === "browser") {
             me.enabled = !me.core.util.isSecure();
         }
         else {
             me.enabled = true;
         }
     };
-    me.formatMessage = function(id, message) {
+    me.formatMessage = function (componentId, userName, message) {
         var date = new Date();
-        var formattedMessage = date.toUTCString() + " log [" + me.platform + (this.id ? " - " + this.id : "") + "] " + message;
+        var formattedMessage = date.toUTCString() + " log [" + me.platform;
+        if (userName) {
+            formattedMessage += " - " + userName;
+        }
+        if (componentId) {
+            formattedMessage += " - " + componentId;
+        }
+        formattedMessage += "] " + message;
         return formattedMessage;
     };
-    me.push = function(message) {
-        if(me.messages.length > me.fixedSize) {
+    me.push = function (message) {
+        if (me.messages.length > me.fixedSize) {
             me.messages.shift();
         }
         me.messages.push(message);
     };
-    me.log = function(message) {
-        if(me.enabled) {
+    me.log = function (message) {
+        if (me.enabled) {
             var fullMessage = me.formatMessage(this.id, message);
             me.push(fullMessage);
             console.log(fullMessage);
         }
     };
-    me.log_warn = function(message) {
-        if(me.enabled) {
+    me.log_warn = function (message) {
+        if (me.enabled) {
             var fullMessage = me.formatMessage(this.id, message);
             me.push(fullMessage);
             console.warn(fullMessage);
         }
     };
-    me.log_error = function(message, stack) {
-        if(me.enabled) {
-            if(message) {
-                if(!stack) {
+    me.log_error = function (message, stack, componentId, userName) {
+        if (me.enabled) {
+            if (message) {
+                if (!stack) {
                     stack = new Error().stack;
                 }
-                var fullMessage = me.formatMessage(this.id, message + " stack: " + stack);
+                if (!componentId) {
+                    componentId = this.id;
+                }
+                if (!userName) {
+                    userName = this.userName;
+                }
+                var fullMessage = me.formatMessage(componentId,
+                    userName,
+                    message + " stack: " + stack);
                 me.push(fullMessage);
                 me.errors.push(fullMessage);
+                if (me.platform !== "server") {
+                    me.core.message.send_server("core.console.log_error",
+                        message,
+                        stack,
+                        componentId,
+                        userName);
+                }
                 console.error(fullMessage);
             }
         }
     };
-    me.isEnabled = function() {
+    me.isEnabled = function () {
         return me.enabled;
     };
-    me.enable = function(flag) {
+    me.enable = function (flag) {
         me.enabled = flag;
     };
-    me.retrieveMessages = function() {
+    me.retrieveMessages = function () {
         return me.messages;
     };
-    me.retrieveErrors = function() {
+    me.retrieveErrors = function () {
         return me.errors;
     };
-    me.clearMessages = function() {
+    me.clearMessages = function () {
         me.clear();
     };
 };
