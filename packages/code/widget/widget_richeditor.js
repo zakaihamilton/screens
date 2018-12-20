78424ccc51b76128748c7ac54711cfc891dd9dd1
Zakai Hamilton <zakaihamilton@gmail.com>
2018-12-20 11:22:00 +0200

    added tooltips

--- packages/code/widget/widget_richeditor.js
+++ packages/code/widget/widget_richeditor.js
@@ -35,19 +35,48 @@ screens.widget.richeditor = function WidgetRichEditor(me) {
                                 ["clean"]
                             ],
                             handlers: {
-                                "speech": function () {
-                                    var result = me.ui.speech.toggle(object);
-                                    var button = me.ui.node.classMember(me.widget.window.get(object), "ql-speech");
-                                    me.core.property.set(button, "ui.class.speechActive", result);
-                                    return result;
-                                }
+                                "speech": () => me.toggleSpeech(object)
                             }
                         }
                     },
                     theme: "snow"
                 });
+                let showTooltip = (el) => {
+                    let name = el.className.replace("ql-", "");
+                    var tooltip = me.core.string.title(name);
+                    if (el.value) {
+                        tooltip = me.core.string.title(el.value) + " " + tooltip;
+                    }
+                    var replacements = {
+                        "1 Header": "Heading 1",
+                        "2 Header": "Heading 2",
+                        "-1 Indent": "Decrease Indentation",
+                        "+1 Indent": "Increase Indentation",
+                        "Rtl Direction": "Text Direction"
+                    };
+                    if (replacements[tooltip]) {
+                        tooltip = replacements[tooltip];
+                    }
+                    el.setAttribute("title", tooltip);
+                };
+
+                var window = me.widget.window.get(object);
+                var toolbar = me.ui.node.classMember(window, "ql-toolbar");
+                if (toolbar) {
+                    let matches = toolbar.querySelectorAll("button");
+                    for (let el of matches) {
+                        showTooltip(el);
+                    }
+                }
             }
         }
+    };
+    me.toggleSpeech = function (object) {
+        var window = me.widget.window.get(object);
+        var result = me.ui.speech.toggle(object);
+        var toolbar = me.ui.node.classMember(window, "ql-toolbar");
+        var button = me.ui.node.classMember(toolbar, "ql-speech");
+        me.core.property.set(button, "ui.class.speechActive", result);
     };
     me.contents = {
         get: function (object) {
