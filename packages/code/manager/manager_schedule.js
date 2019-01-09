fc1ef0d7b4a48bbeeba791b3719c12a29f238b3b
Zakai Hamilton <zakaihamilton@gmail.com>
2019-01-09 16:43:55 +0200

    bug fixes

--- packages/code/manager/manager_schedule.js
+++ packages/code/manager/manager_schedule.js
@@ -40,25 +40,29 @@ screens.manager.schedule = function ManagerSchedule(me) {
             }
         }
         let [public, private] = await me.manager.content.associated(null, this.userId);
-        for (let app in public) {
-            for (let title of public[app]) {
-                let [year, month, day, name] = title.split(/(\d{4})-(\d{2})-(\d{2})\s(.+)/g).slice(1);
-                month = parseInt(month) - 1;
-                let eventDate = me.toDate({ year, month, day });
-                if (startDate <= eventDate && eventDate <= endDate) {
-                    var group = me.core.string.language(title) === "english" ? "American" : "Israel";
-                    events.push({
-                        date: {
-                            year: parseInt(year),
-                            month: parseInt(month),
-                            day: parseInt(day),
-                        },
-                        name,
-                        group,
-                        title,
-                        app: me.core.string.title(app),
-                        launch: [app, title]
-                    });
+        let sources = { public, private };
+        for (var source in sources) {
+            let list = sources[source];
+            for (let app in list) {
+                for (let title of list[app]) {
+                    let [year, month, day, name] = title.split(/(\d{4})-(\d{2})-(\d{2})\s(.+)/g).slice(1);
+                    month = parseInt(month) - 1;
+                    let eventDate = me.toDate({ year, month, day });
+                    if (startDate <= eventDate && eventDate <= endDate) {
+                        var group = me.core.string.language(title) === "english" ? "American" : "Israel";
+                        events.push({
+                            date: {
+                                year: parseInt(year),
+                                month: parseInt(month),
+                                day: parseInt(day),
+                            },
+                            name,
+                            group,
+                            title,
+                            app: me.core.string.title(app),
+                            launch: [app, title, source === "private"]
+                        });
+                    }
                 }
             }
         }
