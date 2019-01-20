8a53de984097c67591c3691ba75f773f8c85a5cd
Zakai Hamilton <zakaihamilton@gmail.com>
2019-01-20 14:48:23 +0200

    propagate script

---
+++ packages/res/scripts/channelLineupWithDetails.js
@@ -0,0 +1,51 @@
+/*
+1. Read channelsLineup.json
+2. Traverse servicePresentationData.services
+3. Read logical channel number
+4. Read template and apply template to each service
+5. Output new file
+*/
+
+var me = me, object = object;
+
+async function run() {
+    var window = me.widget.window.get(object);
+    var channelsLineup = await me.content(window, "channelsLineup.json");
+    var channelDetails = await me.content(window, "channelDetails.json");
+    if (!channelsLineup) {
+        alert("channelsLineup.json not loaded in files or is an empty file");
+        return;
+    }
+    if (!channelDetails) {
+        alert("channelDetails.json not loaded in files or is an empty file");
+        return;
+    }
+    try {
+        var channelsLineup = JSON.parse(channelsLineup);
+    }
+    catch (err) {
+        alert("Invalid json: " + err + " content: " + channelsLineup);
+    }
+    try {
+        var channelDetails = JSON.parse(channelDetails);
+    }
+    catch (err) {
+        alert("Invalid json: " + err + " content: " + channelDetails);
+    }
+    var services = channelsLineup.servicePresentationData.services;
+    services = services.map(service => {
+        let LCN = service.logicalChannelNumber;
+        service = Object.assign(service, channelDetails);
+        let text = JSON.stringify(service);
+        text = text.replace(/{LCN}/g, LCN);
+        service = JSON.parse(text);
+        return service;
+    });
+    channelsLineup.servicePresentationData.services = services;
+    me.output(window, "output.json", JSON.stringify(channelsLineup), true);
+    setTimeout(() => {
+        me.format(window);
+    });
+}
+
+run();
