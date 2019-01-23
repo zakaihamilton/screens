5524f6696f24e33117ec51ba8d1166857b235ff7
Zakai Hamilton <zakaihamilton@gmail.com>
2019-01-24 00:08:47 +0200

    bug fix

--- packages/code/core/core_device.js
+++ packages/code/core/core_device.js
@@ -7,4 +7,7 @@ screens.core.device = function CoreDevice(me) {
     me.isMobile = function () {
         return navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i);
     };
+    me.isNotMobile = function () {
+        return !me.isMobile();
+    };
 };
