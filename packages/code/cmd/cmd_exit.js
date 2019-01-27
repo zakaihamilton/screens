bcb273758c3de92a10ef1fc2ac4f1ae816899485
Zakai Hamilton <zakaihamilton@gmail.com>
2019-01-27 15:36:04 +0200

    exit command

---
+++ packages/code/cmd/cmd_exit.js
@@ -0,0 +1,12 @@
+/*
+    @author Zakai Hamilton
+    @component CmdExit
+*/
+
+screens.cmd.exit = function CmdExit(me) {
+    me.cmd = function (terminal, args) {
+        var window = me.widget.window.get(terminal);
+        me.core.property.set(window, "close");
+        me.core.cmd.exit(terminal);
+    };
+};
