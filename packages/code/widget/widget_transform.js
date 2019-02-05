532bd218aded9e0bd179277f22eca4068d716fd6
Zakai Hamilton <zakaihamilton@gmail.com>
2019-02-05 19:36:26 +0200

    bug fixes

--- packages/code/widget/widget_transform.js
+++ packages/code/widget/widget_transform.js
@@ -111,10 +111,6 @@ screens.widget.transform = function WidgetTransform(me) {
             widget.transformText = value;
         }
     };
-    me.sync = async function (object) {
-        await me.storage.cache.empty();
-        me.transform(object);
-    };
     me.transform = async function (object) {
         var widget = me.findWidget(object);
         me.widget.transform.layout.clear(widget.var.layout);
