358ad58d038baf72836741484f1c0fd6cb244683
Zakai Hamilton <zakaihamilton@gmail.com>
2019-02-11 14:56:00 +0200

    parquet support

--- packages/code/lib/lib_zlib.js
+++ packages/code/lib/lib_zlib.js
@@ -5,17 +5,17 @@
 
 screens.lib.zlib = function LibZLib(me) {
     me.init = function () {
-        me.zlib = require('zlib');
+        me.zlib = require("zlib");
         me.core.property.link("core.http.compress", "lib.zlib.compress", true);
     };
     me.compress = async function (info) {
-        var encoding = info.headers['accept-encoding'];
+        var encoding = info.headers["accept-encoding"];
         var compressMethod = null;
         if (encoding && info.body) {
-            if (encoding.includes('gzip')) {
+            if (encoding.includes("gzip")) {
                 encoding = "gzip";
                 compressMethod = me.zlib.gzip;
-            } else if (encoding.includes('deflate')) {
+            } else if (encoding.includes("deflate")) {
                 encoding = "deflate";
                 compressMethod = me.zlib.inflate;
             }
@@ -26,7 +26,7 @@ screens.lib.zlib = function LibZLib(me) {
                     if (err) {
                         me.log("compress encoding failed for encoding: " + encoding + " error: " + JSON.stringify(err));
                     } else {
-                        info.responseHeaders['Content-Encoding'] = encoding;
+                        info.responseHeaders["Content-Encoding"] = encoding;
                         info.body = buf;
                     }
                     resolve();
