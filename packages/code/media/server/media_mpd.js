41b2693868fa1f8d73364e424d9993c7fdaeeb86
Zakai Hamilton <zakaihamilton@gmail.com>
2019-03-14 14:10:59 +0200

    screens view manifest

--- packages/code/media/server/media_mpd.js
+++ packages/code/media/server/media_mpd.js
@@ -7,19 +7,46 @@ screens.media.mpd = function MediaMPD(me, packages) {
     const { core } = packages;
     core.property.link("core.http.receive", "media.mpd.receive", true);
     me.receive = async function (info) {
+        let { url } = info;
         if (me.platform !== "server" || info.method !== "GET") {
             return;
         }
-        if (info.url.endsWith(".mpd")) {
-            if (info.url.startsWith("/api/mpd/")) {
-                let body = "";
+        if (url.endsWith(".mpd")) {
+            if (url.startsWith("/api/mpd/")) {
+                let minBufferTime = "1.5";
+                let duration = "30";
+                let baseUrl = "";
+                let mimeType = "video/mp2t";
+                let resolution = "720p";
+                let bandwidth = "3200000";
+                let width = "1280";
+                let height = "720";
+                let body = "<?xml version=\"1.0\"?>";
+                body += `<MPD xmlns="urn:mpeg:dash:schema:mpd:2011" profiles="urn:mpeg:dash:profile:full:2011" minBufferTime="PT${minBufferTime}S">`;
+                body += `<Period duration="PT${duration}S">`;
+                body += `<BaseURL>${baseUrl}</BaseURL>`;
+                body += `<AdaptationSet mimeType="${mimeType}">`;
+                body += `<Representation id="${resolution}" bandwidth="${bandwidth}" width="${width}" height="${height}">`;
+                body += `<SegmentList timescale="90000" duration="5400000">`;
+                body += `<RepresentationIndex sourceURL="representation-index.sidx" />`;
+                body += `<SegmentURL media="segment-1.ts" />`;
+                body += `</SegmentList >`;
+                body += `</Representation >`;
+                body += `</AdaptationSet>`;
+                body += `</Period>`;
+                body += `</MPD>`;
+
+
+
+
                 info["content-type"] = " application/dash+xml";
                 info.body = body;
             }
         }
-        if (info.url.startsWith("/api/mpd/stream/") && info.url.endsWith(".ts")) {
+        if (info.url.startsWith("/api/mpd/stream/")) {
             let path = info.url.match(/api\/mpd\/stream\/(.*)/)[1];
-            let ts_file = core.path.folderPath(path) + ".ts";
+            let extension = core.path.extension(path);
+            let sourceFile = core.path.folderPath(path) + "." + extension;
             let offset = parseInt(core.path.fileName(path));
             let size = parseInt(info.query["size"]) | 0;
             let headers = Object.assign({}, info.headers);
@@ -28,7 +55,8 @@ screens.media.mpd = function MediaMPD(me, packages) {
                 headers.total = size;
             }
             info.custom = true;
-            core.stream.serve(headers, info.response, ts_file, "video/MP2T");
+            var mimeType = me.mime.getType(extension);
+            core.stream.serve(headers, info.response, sourceFile, mimeType);
         }
     };
     me.genParts = function (source, params) {
