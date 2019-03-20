16b77222a1ad462e3b69211e26f27eca75922641
Zakai Hamilton <zakaihamilton@gmail.com>
2019-03-20 10:41:30 +0200

    bug fix

--- packages/code/modal/modal_question.js
+++ packages/code/modal/modal_question.js
@@ -10,14 +10,22 @@ screens.modal.question = function ModalQuestion(me, packages) {
         window.promise = new Promise((resolve, reject) => {
             window.resolve = resolve;
             window.reject = reject;
+            window.close = reject;
         });
+        core.property.set(window, "app", me);
         return window.promise;
     };
     me.action = function (object, action) {
         let window = widget.window.get(object);
         if (action in window) {
+            window.close = window[action];
             core.property.set(window, "close");
-            window[action]();
+        }
+    };
+    me.close = function (object) {
+        let window = widget.window.get(object);
+        if (window.close) {
+            window.close();
         }
     };
 };
