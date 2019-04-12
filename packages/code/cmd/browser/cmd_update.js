38aa2efae8f86658e63b0cda22daa4d4d57528d8
Zakai Hamilton <zakaihamilton@gmail.com>
2019-04-12 18:21:21 +0300

    update cmd

---
+++ packages/code/cmd/browser/cmd_update.js
@@ -0,0 +1,13 @@
+/*
+    @author Zakai Hamilton
+    @component CmdUpdate
+*/
+
+screens.cmd.update = function CmdSize(me, packages) {
+    const { core, db } = packages;
+    me.cmd = async function (terminal, args) {
+        await db.events.msg.send("core.server.run", "git pull");
+        core.property.set(terminal, "print", "sent update");
+        core.cmd.exit(terminal);
+    };
+};
