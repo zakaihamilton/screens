a615718c0b450fc79bee52f428629092d3c18b68
Zakai Hamilton <zakaihamilton@gmail.com>
2019-04-13 22:44:06 +0300

    bug fix

--- packages/code/cmd/browser/cmd_update.js
+++ packages/code/cmd/browser/cmd_update.js
@@ -8,6 +8,8 @@ screens.cmd.update = function CmdSize(me, packages) {
     me.cmd = async function (terminal, args) {
         await db.events.msg.send("core.server.run", "git checkout package.json");
         await db.events.msg.send("core.server.run", "git pull");
+        await db.events.msg.send("core.server.run", "npm install");
+        await db.events.msg.send("core.server.run", "npm rebuild");
         core.property.set(terminal, "print", "sent update");
         core.cmd.exit(terminal);
     };
