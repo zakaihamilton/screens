7c8ed4ec974168d338073b4fd24c138c65650fd7
Zakai Hamilton <zakaihamilton@gmail.com>
2019-04-20 21:56:26 +0300

    bug fix

--- packages/code/cmd/browser/cmd_state.js
+++ packages/code/cmd/browser/cmd_state.js
@@ -9,6 +9,7 @@ screens.cmd.state = function CmdState(me, packages) {
         var items = await db.events.state.list();
         if (items) {
             for (let item of items) {
+                delete item._id;
                 core.property.set(terminal, "print", JSON.stringify(item));
             }
         }
