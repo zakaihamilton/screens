cd8db6f649c614d299bfe602e3c646cad6fbe2a0
Zakai Hamilton <zakaihamilton@gmail.com>
2019-05-15 00:59:33 +0300

    coding

--- components/ui/widget/ui_widget_custom.js
+++ components/ui/widget/ui_widget_custom.js
@@ -4,20 +4,58 @@ COMPONENT.define({
         platform: "browser",
     },
     init(me) {
-
+    },
+    stylesToHtml(element) {
+        let data = element.data;
+        if (!data.styles) {
+            return "";
+        }
+        let styles = "";
+        for (let key in data.styles) {
+            styles += key + ":" + data.styles[key] + ";";
+        }
+        return `
+        <style>
+            ${this.config.tag} {
+                ${styles}
+            }
+        </style>`;
     },
     extends(me) {
         window.customElements.define(me.config.tag, class extends HTMLElement {
+            constructor() {
+                super();
 
+                let instance = new COMPONENT[me.name]();
+                instance.element = this;
+                this.instance = instance;
+                let data = typeof instance.data === "function" ? instance.data(this) : instance.data;
+                this.data = data;
+                let html = "";
+                let styles = instance.stylesToHtml(this);
+                if (styles) {
+                    html += styles;
+                }
+                let content = typeof instance.render === "function" ? instance.render(this) : instance.render;
+                if (content) {
+                    html += content;
+                }
+                if (html) {
+                    this.innerHTML = html;
+                }
+            }
         });
-    },
-    constructor() {
-        this.element = document.createElement(this.config.tag);
     },
     show() {
         this.element.style.visibility = "visible";
     },
     hide() {
         this.element.style.visibility = "hidden";
+    },
+    render() {
+
+    },
+    data() {
+        return {};
     }
 });
--- components/ui/widget/ui_widget_window.js
+++ components/ui/widget/ui_widget_window.js
@@ -5,7 +5,28 @@ COMPONENT.define({
         extends: "UIWidgetCustom",
         tag: "widget-window"
     },
-    constructor() {
-        this.element.innerHTML = "<h1>Hello</h1>";
+    data: {
+        styles: {
+            width: "500px",
+            height: "100px",
+            border: "1px solid black",
+            "background-color": "red",
+            display: "block"
+        },
+        title: "Hello",
+    },
+    render: element => `
+        <widget-window-title>${element.data.title}</widget-window-title>`
+});
+
+COMPONENT.define({
+    name: "UIWidgetTitle",
+    config: {
+        platform: "browser",
+        extends: "UIWidgetCustom",
+        tag: "widget-window-title"
+    },
+    render() {
+
     }
 });
