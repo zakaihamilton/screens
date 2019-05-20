a1f56cccab93bce7750b0c4df529763140229994
Zakai Hamilton <zakaihamilton@gmail.com>
2019-05-20 16:01:32 +0300

    bug fix

--- components/ui/widget/ui_widget_menu.js
+++ components/ui/widget/ui_widget_menu.js
@@ -7,14 +7,19 @@ COMPONENT.UIWidgetMenu = class extends COMPONENT.UIWidget {
     }
     normal() {
         return {
-            "border-bottom": "1px solid black",
-            "width": "100%",
-            "height": "300px",
-            "overflow": "scroll"
+            "border-top": "1px solid black",
+            "border-right": "1px solid black",
+            "background-color": "red",
+            "width": "70%",
+            "height": "100%",
+            "top": "31px",
+            "overflow": "scroll",
+            "box-sizing": "border-box",
+            "position": "absolute"
         };
     }
     render(element) {
-        let html = "<div>Hello</div><div>Yo!</div>";
+        let html = "<div>Hello</div><div>Yo!</div><div>Hello</div><div>Yo!</div><div>Hello</div><div>Yo!</div><div>Hello</div><div>Yo!</div>";
         return html;
     }
 };
