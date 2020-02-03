d7b9c9e3aef72b690953013fb3d09008569b1034
Zakai Hamilton <zakaihamilton@gmail.com>
2020-02-04 00:55:50 +0200

    bug fix

--- packages/code/react/browser/swimlane/react_swimlane.js
+++ packages/code/react/browser/swimlane/react_swimlane.js
@@ -44,7 +44,7 @@ screens.react.Swimlane.Item = ({ image, offset, title, horizontal, children, ite
     else {
         props.style.left = offset + unit;
     }
-    const titleOverlay = title.split("-").map(text => (<Element>{text}</Element>));
+    const titleOverlay = title.split("-").map((text, index) => (<Element key={index}>{text}</Element>));
     return (
         <Element className="react-swimlane-item-container" {...props}>
             <Element className="react-swimlane-item-title">{title}</Element>
