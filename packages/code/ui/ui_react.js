692a62843f0d18e7043dca8024c98832a1903ae1
Zakai Hamilton <zakaihamilton@gmail.com>
2020-02-13 21:57:43 +0200

    bug fix

--- packages/code/ui/ui_react.js
+++ packages/code/ui/ui_react.js
@@ -4,6 +4,13 @@
  */
 
 screens.ui.react = function UIReact(me, { core, react }) {
+    me.init = function () {
+        for (const key in react) {
+            if (typeof react[key] === "function") {
+                react[key].displayName = key;
+            }
+        }
+    };
     me.render = async function (object, method) {
         const component = await core.property.get(object, method);
         const element = react.util.render(object, component);
