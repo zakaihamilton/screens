c7d1e1d4c099b6faaabe528872468ec28bb60fa6
Zakai Hamilton <zakaihamilton@gmail.com>
2020-03-31 14:33:21 +0300

    command fix

--- packages/code/cmd/browser/cmd_servers.js
+++ packages/code/cmd/browser/cmd_servers.js
@@ -4,20 +4,26 @@
 */
 
 screens.cmd.servers = function CmdServer(me, { core, db }) {
-    me.cmd = async function (terminal) {
-        try {
-            var servers = await db.events.servers.list({});
-            var ipList = servers.map(server => server.ip).filter(Boolean);
-            for (const ip of ipList) {
-                core.property.set(terminal, "print", ip);
-            }
+    me.cmd = async function (terminal, args) {
+        if (args[1]) {
+            core.property.set(terminal, "print", args.slice(1).join(" "));
+            await db.events.msg.send(...args.slice(1));
         }
-        catch (err) {
-            let error = err;
-            if (typeof error !== "string") {
-                error = JSON.stringify(err);
+        else {
+            try {
+                var servers = await db.events.servers.list({});
+                var ipList = servers.map(server => server.ip).filter(Boolean);
+                for (const ip of ipList) {
+                    core.property.set(terminal, "print", ip);
+                }
             }
-            core.property.set(terminal, "print", error);
+            catch (err) {
+                let error = err;
+                if (typeof error !== "string") {
+                    error = JSON.stringify(err);
+                }
+                core.property.set(terminal, "print", error);
+            }
         }
         core.cmd.exit(terminal);
     };
