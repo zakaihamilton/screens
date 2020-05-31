5e72d4aa890c790ec498c3fae59febfe0cc362b9
Zakai Hamilton <zakaihamilton@gmail.com>
2020-05-31 13:42:16 +0300

    term fixes copy and move support in fs

--- packages/code/storage/storage_fs.js
+++ packages/code/storage/storage_fs.js
@@ -31,6 +31,17 @@ screens.storage.fs = function StorageFS(me, { }) {
             };
         });
     };
+    me.exists = async function (path) {
+        let result = undefined;
+        try {
+            const stat = await me.stat(path);
+            result = true;
+        }
+        catch (err) {
+            result = false;
+        }
+        return result;
+    };
     me.delete = async function (path) {
         const stat = await me.stat(path);
         if (stat.isDirectory()) {
@@ -46,22 +57,51 @@ screens.storage.fs = function StorageFS(me, { }) {
     };
     me.list = async function (path) {
         const items = [];
-        const names = await me.readdir(path);
-        for (const name of names) {
-            const item = { name, path: path + "/" + name };
-            const stat = await me.stat(item.path);
-            if (stat.isDirectory()) {
-                item.type = "folder";
+        const stat = await me.stat(path);
+        if (stat.isDirectory()) {
+            const names = await me.readdir(path);
+            for (const name of names) {
+                const item = { name, path: path + "/" + name };
+                const stat = await me.stat(item.path);
+                if (stat.isDirectory()) {
+                    item.type = "folder";
+                }
+                else if (stat.isFile()) {
+                    item.type = "file";
+                }
+                else if (stat.isSymbolicLink()) {
+                    item.type = "link";
+                }
+                items.push(item);
             }
-            else if (stat.isFile()) {
-                item.type = "file";
-            }
-            else if (stat.isSymbolicLink()) {
-                item.type = "link";
-            }
-            items.push(item);
         }
         return items;
+    };
+    me.copy = async function (source, target) {
+        const stat = await me.stat(source);
+        if (stat.isDirectory()) {
+            if (!me.exists(target)) {
+                me.mkdir(target);
+            }
+            const names = me.readdir(source);
+            for (const name of names) {
+                const path = source + "/" + name;
+                me.copy(path, target + "/" + name);
+            }
+        }
+        else {
+            if (me.platform === "server") {
+                await me.fs.promises.copyFile(source, target);
+            }
+            else {
+                const data = me.readFile(source);
+                me.writeFile(target, data);
+            }
+        }
+    };
+    me.move = async function (source, target) {
+        await me.copy(source, target);
+        await me.delete(source);
     };
 };
 
