a4ef470bed879c2d0941003ce6c75f1b5dfe266b
Zakai Hamilton <zakaihamilton@gmail.com>
2020-06-23 23:42:49 +0300

    bug fix

--- packages/code/storage/storage_listing.js
+++ packages/code/storage/storage_listing.js
@@ -4,17 +4,22 @@
  */
 
 screens.storage.listing = function StorageListing(me, { storage, media }) {
-    me.init = async () => {
-        me.metadata = "aws/" + await media.file.bucket() + "/metadata";
+    me.metadataPath = async () => {
+        if (!me.metadata) {
+            me.metadata = "aws/" + await media.file.bucket() + "/metadata";
+        }
+        return me.metadata;
     };
     me.update = async path => {
+        const metadataPath = await me.metadataPath();
         const listing = await storage.fs.list(path);
-        await storage.fs.createPath(me.metadata, me.metadata + "/" + path);
-        await storage.fs.writeFile(me.metadata + "/" + path + "/listing.json", JSON.stringify(listing, null, 4), "utf8");
+        await storage.fs.createPath(metadataPath, metadataPath + "/" + path);
+        await storage.fs.writeFile(metadataPath + "/" + path + "/listing.json", JSON.stringify(listing, null, 4), "utf8");
         return listing;
     };
     me.get = async path => {
-        const listingPath = me.metadata + "/" + path + "/listing.json";
+        const metadataPath = await me.metadataPath();
+        const listingPath = metadataPath + "/" + path + "/listing.json";
         if (!await storage.fs.exists(listingPath)) {
             return await me.update(path);
         }
